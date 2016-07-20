6cbff411d528d0450088f98556e4fc1e35fdc3ad
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2016-07-19 20:19:05 -0400

    Add temporary implementation for ConvoRow

--- WhosApp/Components/ConvoView.js
+++ WhosApp/Components/ConvoView.js
@@ -6,11 +6,15 @@ import {
   View
 } from 'react-native';
 
+import ConvoRow from './ConvoRow';
+
 export default class ConvoView extends Component {
   render() {
+    messagesTemp = ['message1', 'message2'];
     return (
       <View>
         <Text style={styles.red}>just red</Text>
+        <ConvoRow emoji='emoji' messages={messagesTemp} />
       </View>
     );
   }
