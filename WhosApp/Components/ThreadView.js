5f1ebf3fee6b4071d5635a892a178049108817da
Rebecca Yuen <rebyuen@cs.washington.edu>
2016-07-19 22:14:48 -0400

    Added ThreadRow for list items in chat threads page

--- WhosApp/Components/ThreadView.js
+++ WhosApp/Components/ThreadView.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+import ThreadRow from './ThreadRow';
+
 export default class ThreadView extends Component {
+  // Initialize hardcoded data
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        {emoji: '🙈', name: 'Lardberries1', preview: 'Where is everyone?', date: 'Jun 27', read: false},
+        {emoji: '🐸', name: 'Lardberries2', preview: 'Ayooooo', date: 'Jun 24', read: true},
+        {emoji: '🦁', name: 'Lardberries3', preview: 'Guys, I heard Jake is a Brony', date: 'Jun 24', read: true},
+      ])
+    };
+  }
+
   render() {
     return (
-      <View style={styles.fillerText}>
-        <Text>Chat Threads!</Text>
+      <View style={{ flex: 1 }}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <ThreadRow threads={rowData} />}
+        />
       </View>
     );
   }
 }
-
-// Styles for testing
-const styles = StyleSheet.create({
-  fillerText: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-});
