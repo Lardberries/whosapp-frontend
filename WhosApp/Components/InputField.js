bdb7638926e51d3bddd4c2a5890d968dcffa12f4
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2016-09-25 22:10:21 -0400

    Add initial InputField file.

---
+++ WhosApp/Components/InputField.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+// Will take a header, the next field reference (for focusing)
+export default class InputField extends Component {
+  focus = () => {
+    this.refs['textInput'].focus();
+  };
+
+  render() {
+    return (
+      <View style={styles.field}>
+        <Text style={styles.fieldHeader}>{this.props.header}</Text>
+        <TextInput
+          ref='textInput'
+          style={styles.fieldInput}
+          autoCapitalize='none'
+          autoCorrect={false}
+          onChangeText={this.props.onChangeText}
+          blurOnSubmit={false}
+          returnKeyType='next'
+          onSubmitEditing={this.props.onSubmitEditing}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  field: {
+    marginBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+    paddingLeft: 2,
+    paddingRight: 2
+  },
+  fieldHeader: {
+    fontFamily: 'Avenir',
+    fontWeight: '500',
+    fontSize: 11,
+    letterSpacing: 0.9,
+    color: '#9A9FA7',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  fieldInput: {
+    height: 30,
+    width: 270,
+    flex: 1,
+    fontFamily: 'Avenir',
+    fontWeight: '500',
+    fontSize: 17,
+  },
+});
